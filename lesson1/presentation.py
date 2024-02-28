43971c2bfc573ecededb32e2e4be66add1571e6d
dvorkam@gmail.com <dvorkam@gmail.com>
2024-02-28 17:02:49 +0100

    Updated examples

--- lesson1/presentation.py
+++ lesson1/presentation.py
@@ -1,76 +1,184 @@
+def print_header(title):
+    print()
+    print(title.center(80, "#"))
+    #print("\n"+"#"*30+" " + f"{title}" +" "+ "#"*30)
+
+
 ### 1. Introduction to Variables and Strings
-print("\n"+"#"*30 + "String demonstration" + "#"*30)
+
 # Introduce a variable storing Taylor Swift's name
+print_header("String demonstration")
 artist_name = "Taylor Swift"
 print(artist_name)
 
 # A string literal representing one of her album titles
 album_title = "Folklore"
 print("One of " + artist_name + "'s albums is called " + album_title + ".")
+album_message = "One of " + artist_name + "'s albums is called " + album_title + "."
+print(album_message)
+
+album_message2 = f"One of {artist_name}'s albums is called {album_title}."
+print(album_message2)
+print(album_message2.upper())
+print(album_message2.capitalize())
+print(album_message2.partition("albums"))
 
 
 ### 2. Working with Numbers (Integers and Floats)
-print("\n"+"#"*30 + "Integer demonstration" + "#"*30)
-# Year Taylor Swift was born
-year_of_birth = 1989
-print("Taylor Swift was born in", year_of_birth)
+print_header("Integer demonstration")
+year_of_birth = 1990
+current_year = 2024
 
-print("\n"+"#"*30 + "Float demonstration" + "#"*30)
+age = current_year - year_of_birth
+voting_since = year_of_birth + 18 #Legal age for voting and (alcohol purchase) in Czechia
+leap_years_experienced = (age-18)//4
+
+
+print(f"I am now {age}, and since I could vote, I have experienced {leap_years_experienced} leap years")
+print(f"I was using {type(age)}, to represent the numbers.")
+
+print_header("Float demonstration")
 # The length of one of her songs in minutes (floating point number)
 song_length = 3.45  # Length of "Cardigan" in minutes
-print("The song 'Cardigan' is", song_length, "minutes long.")
+leap_years_experienced = (age-18)/4
+print(f"Had I used simple division, answer would be {leap_years_experienced}")
+print(f"This number would be of type {type(leap_years_experienced)}")
+
+infinity = float("inf")
+large_number = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+print("Is infinity larger than large number?",infinity>large_number)
 
 
-### 3. Boolean Values
-print("\n"+"#"*30 + "Boolean demonstration" + "#"*30)
-# A boolean value representing whether Taylor Swift is a singer
-is_singer = True
-print("Is Taylor Swift a singer?", is_singer)
-
-# Another boolean representing whether she has won a Grammy
-has_won_grammy = True
-print("Has Taylor Swift won a Grammy?", has_won_grammy)
-
-### 4. Containers
+### 4. Sequences
 #### 4.1. Lists
-print("\n"+"#"*30 + "List demonstration" + "#"*30)
-# List of Taylor Swift's genres
-genres = ["Pop", "Country", "Indie Folk"]
-print("Taylor Swift has performed in these genres:", genres)
+print_header("List demonstration")
+# List of some programming languages
+langs = ["Python", "C++", "Rust", "C", "FizzBuzz", "R", "mathscript"]
+print("These are programming languages listed:", langs)
 
-# Adding a new genre
-genres.append("Electropop")
-print("Updated genres:", genres)
+# Adding a new language
+langs.append("C#")
+print("Updated langueages:", langs)
+
+# Removing a language
+langs.remove("C")
+print("Updated langueages:", langs)
+
+#indexing example: 
+print(f"printing nth (0) element: {langs[0]}")
+
+# Slicing examples
+print(f"printing nth to mth [1:4]  element: {langs[1:4]}")
+print(f"printing fist to mth [:4]  element: {langs[:4]}")
+print(f"printing nth to end [4:] element: {langs[4:]}")
+print(f"printing nth to 2nd to end [2:-2] element: {langs[2:-2]}")
+
+# +operator 
+print(f"Using + to concatenate {[langs[0]]+langs[:2]+langs[5:]}")
 
 
-#### 4.2. Tuples
-print("\n"+"#"*30 + "Tuple demonstration" + "#"*30)
-# A tuple storing the names of Taylor Swift's first three albums
-first_three_albums = ("Taylor Swift", "Fearless", "Speak Now")
-print("Taylor Swift's first three albums are:", first_three_albums)
+#### 4.2 Tuples
+print_header("Tuple demonstration")
+# A tuple storing information about me
+about_me = ("Michal", 42, True)
+print(f"Name: {about_me[0]}, Favorite Number: {about_me[1]}, plays DnD: {about_me[2]} ")
 
-# Accessing elements in a tuple
-print("Her second album is:", first_three_albums[1])
-
+#### 4.3 Ranges
+start = 1
+stop = 12
+step = 3
+my_range = range(start, stop, step)
+print([ele for ele in my_range])
+print(f"2nd element in range is {my_range[2]}")
 
 #### 4.3 Sets
-print("\n"+"#"*30 + "Set demonstration" + "#"*30)
-# A set of genres Taylor Swift has explored
-genres = {"Pop", "Country", "Indie Folk", "Country", "Pop"}
-print("Unique genres Taylor Swift has explored:", genres)
+print_header("Set demonstration")
+# Subset of pokemons
+pokemons = {"Bulbasaur", "Charmander", "Squirtle", "Charmander"}
+print("Unique starting pokemons:", pokemons)
+pokemons.add("Pikachu")
+print("Updated starting pokemons after adding Pikachu:", pokemons)
+pokemons.add("Charmander")
+print("Updated starting pokemons after adding Charmander:", pokemons)
+pokemons.add("Saitama")
+print("Updated starting pokemons after adding Saitama:", pokemons)
+pokemons.remove("Saitama")
+print("Updated starting pokemons after removing Saitama:", pokemons)
 
-# Adding a new genre to the set
-genres.add("Electropop")
-print("Updated genres with Electropop added:", genres)
+official_starting_pokemons= frozenset({"Bulbasaur", "Charmander", "Squirtle", "Charmander"})
 
-#### 4.4. Dictionaries
-print("\n"+"#"*30 + "Dictionary demonstration" + "#"*30)
+imutable_tuple = (1, "john", True, pokemons)
+print(imutable_tuple)
+
+
+
+#### 4.4. Mapping types
+print_header("Dictionary demonstration")
 # Dictionary storing Taylor Swift's album release years
-album_release_years = {
-    "Fearless": 2008,
-    "1989": 2014,
-    "Lover": 2019,
-    "Folklore": 2020,
+favorite_things = {
+    "Movie": "Wag the dog",
+    "TV series": "Scrubs",
+    "Number": 42,
+    "Song": "What I've done",
+    "Shoes": False
 }
+print(f"My favorite things: {favorite_things}")
+print(f"My favorite movie: {favorite_things["Movie"]}")
+favorite_things["Movie"] = "Avengers: Infinity War"
+print(f"After changing my favorite movie: {favorite_things["Movie"]}")
+favorite_things["Language"] = "Chinese"
+print(f"After adding my favorite language: {favorite_things}")
 
-print("The album 'Folklore' was released in", album_release_years["Folklore"])
+
+### 5. Boolean Values and NoneType
+print_header("Boolean demonstration")
+# A boolean value representing facts about TS
+am_I_always_lying = True
+is_that_paradox = True
+
+#answer = input("So am I lying or not?: ")
+#print(type(answer))
+
+
+### 6. Special operators
+list1 = [1, 2, 3, 4, 5]
+list2 = [1, 2, 3, 4, 5]
+list3 = list1
+list4 = list1[:]
+
+print(f"is list1 list 2", list1 is list2)
+print(f"is list1 list 3", list1 is list3)
+print(f"is list1 list 4", list1 is list4)
+
+### 7. Flow controll
+### 7.1 Flow controll if else elif
+list = [1,32,68]
+if 68 in list:
+    print("there is 68 in list")
+elif 32 in list:
+    print("there is 32 in list")
+else:
+    print("Neither 32 or 68 are in the list")
+
+
+### 7.2 Flow controll for cycle
+for pokemon in pokemons:
+    print(pokemon)
+
+for val in range(5):
+    print(val)
+
+print()
+### 7.2 Flow controll while cycle
+idx = 0
+while(idx<10):
+    print(f"{idx}- Still cycling")
+    idx+=1
+
+### 8 Functions
+def this_is_function(this_is_argument, this_is_second_argument):
+    return this_is_argument * this_is_second_argument
+
+print(this_is_function("Hello", 10))
+print(this_is_function(10, 10))
+print(this_is_function(False, 10))
